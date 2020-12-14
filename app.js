83b831f0dc89369ba8b49e4471cfc3e25da844a4
kleitonalbuquerque <kleiton2102@gmail.com>
2020-12-14 19:06:11 -0300

    Seção 1 - Módulos

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var mostrarSite = true;
+/*var mostrarSite = true;
 const site = 'https://klmtech.inf.br/';
 
 console.log('Hello World!');
@@ -7,4 +7,14 @@ console.log('E eu estou aprendendo Node.js com o Guia do Programandor');
 
 if(mostrarSite) {
     console.log(site);
-}
+}*/
+
+/*
+* Módulos
+*/
+var calculadora =  require('./calculadora');
+
+console.log(calculadora.soma(10,20));
+console.log(calculadora.mult(10,20));
+calculadora.nome = 'Calculadora do Kleiton!';
+console.log(calculadora.nome);
