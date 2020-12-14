460804370a08c7b792eb8660f7a485543e26ad1d
kleitonalbuquerque <kleiton2102@gmail.com>
2020-12-14 18:34:52 -0300

    Primeiro código com Node

---
+++ app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = 'https://klmtech.inf.br/';
+
+console.log('Hello World!');
+console.log('Meu nome é Kleiton!');
+console.log('E eu estou aprendendo Node.js com o Guia do Programandor');
+
+if(mostrarSite) {
+    console.log(site);
+}
