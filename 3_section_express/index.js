b75880c1a453a23982b22c7531228cfa275f0e86
kleitonalbuquerque <kleiton2102@gmail.com>
2020-12-15 13:38:05 -0300

    Rotas

--- 3_section_express/index.js
+++ 3_section_express/index.js
@@ -1,6 +1,19 @@
 const express = require('express'); // Importando o express
 const app = express(); // Inciando o express
 
+// rotas
+app.get("/", (req, res) => {
+    res.send('Bem vindo ao guia do programador!');
+});
+
+app.get("/blog", (req, res) => {
+    res.send('Bem vindo ao meu blog!');
+});
+
+app.get("/canal/youtube", (req, res)=> {
+    res.send('<h1>Bem vindo ao meu canal!</h1>');
+})
+
 app.listen(4000,(erro) => {
     if(erro) {
         console.log('Ops, algo deu errado!')
